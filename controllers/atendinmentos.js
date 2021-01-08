5012210781cd01efec0740c77513c8220a58e606
Luis Santos <lsantos.ls@outlook.com>
2021-01-08 00:55:27 -0300

    Finalização do curos 1 da formação node.js da Alura

---
+++ controllers/atendinmentos.js
@@ -0,0 +1,34 @@
+const Atendimentos = require('../models/Atendimentos');
+
+module.exports = app => {
+
+    app.get('/atendimentos', (req, res) => {
+        Atendimentos.lista(res);
+    });
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        Atendimentos.buscaPorId(id, res);
+    })
+
+    app.post('/atendimentos', (req, res) => {
+
+        const atendimentos = req.body;
+
+        Atendimentos.adiciona(atendimentos, res);
+
+    })
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        const valores = req.body;
+
+        Atendimentos.altera(id, valores, res);
+    });
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        
+        Atendimentos.deleta(id, res);
+    });
+}
